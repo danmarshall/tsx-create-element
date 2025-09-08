2aee6da65b173e604a261420327c82341c00451a
Dan Marshall <danmar@microsoft.com>
2025-09-08 08:31:58 -0700

    regen

--- docs/index.js
+++ docs/index.js
@@ -244,11 +244,23 @@ function recreateWithSvgNamespace(element) {
   for (var i = 0; i < element.attributes.length; i++) {
     var attr = element.attributes[i];
     svgElement.setAttributeNS(null, attr.name, attr.value);
+  } // Copy event handlers and other properties
+  // Common event handlers that need to be copied
+
+
+  var eventProperties = ['onclick', 'onmousedown', 'onmouseup', 'onmouseover', 'onmouseout', 'onmousemove', 'onkeydown', 'onkeyup', 'onkeypress', 'onfocus', 'onblur'];
+
+  for (var _i = 0, _eventProperties = eventProperties; _i < _eventProperties.length; _i++) {
+    var prop = _eventProperties[_i];
+
+    if (element[prop]) {
+      svgElement[prop] = element[prop];
+    }
   } // Copy children recursively
 
 
-  for (var _i2 = 0; _i2 < element.childNodes.length; _i2++) {
-    var child = element.childNodes[_i2];
+  for (var _i3 = 0; _i3 < element.childNodes.length; _i3++) {
+    var child = element.childNodes[_i3];
 
     if (child.nodeType === Node.ELEMENT_NODE) {
       svgElement.appendChild(recreateWithSvgNamespace(child));
@@ -376,9 +388,7 @@ function tagNamespace(tag) {
 },{}],"VMCN":[function(require,module,exports) {
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+exports.__esModule = true;
 exports.SubComponent = void 0;
 
 var index_1 = require("../dist/es6/index");
@@ -388,13 +398,13 @@ var SubComponent = function SubComponent(props) {
     return props.textAreaChange(e.currentTarget);
   };
 
-  return (0, index_1.createElement)("div", {
+  return index_1.createElement("div", {
     style: {
       border: "1px solid black",
       margin: "0.5em 0",
       padding: "0.5em"
     }
-  }, (0, index_1.createElement)("div", null, "Text is: ", props.someText), (0, index_1.createElement)("div", null, "Children:", (0, index_1.createElement)("div", null, props.children)), (0, index_1.createElement)("textarea", {
+  }, index_1.createElement("div", null, "Text is: ", props.someText), index_1.createElement("div", null, "Children:", index_1.createElement("div", null, props.children)), index_1.createElement("textarea", {
     onKeyPress: textChangeHandler,
     onKeyUp: textChangeHandler,
     onKeyDown: textChangeHandler,
@@ -406,73 +416,69 @@ exports.SubComponent = SubComponent;
 },{"../dist/es6/index":"St5X"}],"G5/u":[function(require,module,exports) {
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+exports.__esModule = true;
 exports.SvgNamespaceTest = void 0;
 
 var index_1 = require("../dist/es6/index"); // Test for SVG namespace fix with overlapping tags
 
 
 var SvgNamespaceTest = function SvgNamespaceTest() {
-  return (0, index_1.createElement)("div", null, (0, index_1.createElement)("h2", null, "SVG Namespace Test"), (0, index_1.createElement)("p", null, "Testing overlapping HTML/SVG tags inside SVG elements:"), (0, index_1.createElement)("svg", {
+  return index_1.createElement("div", null, index_1.createElement("h2", null, "SVG Namespace Test"), index_1.createElement("p", null, "Testing overlapping HTML/SVG tags inside SVG elements:"), index_1.createElement("svg", {
     width: "300",
     height: "200",
     style: {
       border: '1px solid black'
     }
-  }, (0, index_1.createElement)("rect", {
+  }, index_1.createElement("rect", {
     x: "10",
     y: "10",
     width: "80",
     height: "80",
     fill: "red"
-  }), (0, index_1.createElement)("circle", {
+  }), index_1.createElement("circle", {
     cx: "150",
     cy: "50",
     r: "30",
     fill: "blue"
-  }), (0, index_1.createElement)("line", {
+  }), index_1.createElement("line", {
     x1: "10",
     y1: "100",
     x2: "100",
     y2: "100",
     stroke: "green",
     strokeWidth: "2"
-  }), (0, index_1.createElement)("a", {
+  }), index_1.createElement("a", {
     href: "#test"
-  }, (0, index_1.createElement)("title", null, "SVG Title Element"), (0, index_1.createElement)("text", {
+  }, index_1.createElement("title", null, "SVG Title Element"), index_1.createElement("text", {
     x: "10",
     y: "130",
     fill: "purple"
-  }, "Clickable SVG Text")), (0, index_1.createElement)("style", null, ".svg-style { fill: orange; }"), (0, index_1.createElement)("a", {
+  }, "Clickable SVG Text")), index_1.createElement("style", null, ".svg-style { fill: orange; }"), index_1.createElement("a", {
     href: "#link1"
-  }, (0, index_1.createElement)("text", {
+  }, index_1.createElement("text", {
     x: "10",
     y: "170",
     fill: "red"
-  }, "SVG Link 1")), (0, index_1.createElement)("a", {
+  }, "SVG Link 1")), index_1.createElement("a", {
     href: "#link2"
-  }, (0, index_1.createElement)("text", {
+  }, index_1.createElement("text", {
     x: "150",
     y: "170",
     fill: "blue"
-  }, "SVG Link 2"))), (0, index_1.createElement)("p", null, (0, index_1.createElement)("strong", null, "All elements above should use SVG namespace (http://www.w3.org/2000/svg)")), (0, index_1.createElement)("h3", null, "HTML Elements (for comparison)"), (0, index_1.createElement)("p", null, "Compare with HTML elements which use HTML namespace:"), (0, index_1.createElement)("a", {
+  }, "SVG Link 2"))), index_1.createElement("p", null, index_1.createElement("strong", null, "All elements above should use SVG namespace (http://www.w3.org/2000/svg)")), index_1.createElement("h3", null, "HTML Elements (for comparison)"), index_1.createElement("p", null, "Compare with HTML elements which use HTML namespace:"), index_1.createElement("a", {
     href: "#html-link1"
-  }, "HTML Link 1"), (0, index_1.createElement)("a", {
+  }, "HTML Link 1"), index_1.createElement("a", {
     href: "#html-link2"
-  }, "HTML Link 2"), (0, index_1.createElement)("a", {
+  }, "HTML Link 2"), index_1.createElement("a", {
     href: "#html-link3"
-  }, "HTML Link 3"), (0, index_1.createElement)("p", null, "Before the fix: overlapping tags like <a>, <title>, <style> would use HTML namespace"), (0, index_1.createElement)("p", null, "After the fix: all elements inside <svg> automatically use SVG namespace"));
+  }, "HTML Link 3"), index_1.createElement("p", null, "Before the fix: overlapping tags like <a>, <title>, <style> would use HTML namespace"), index_1.createElement("p", null, "After the fix: all elements inside <svg> automatically use SVG namespace"));
 };
 
 exports.SvgNamespaceTest = SvgNamespaceTest;
 },{"../dist/es6/index":"St5X"}],"/RRg":[function(require,module,exports) {
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+exports.__esModule = true;
 exports.StatefulSvgTest = void 0;
 
 var index_1 = require("../dist/es6/index"); // Test for SVG namespace fix with stateful functionality
@@ -489,44 +495,44 @@ var StatefulSvgTest = function StatefulSvgTest(props) {
 
     var hue = i * 30 % 360; // Different color for each circle
 
-    circles.push((0, index_1.createElement)("circle", {
+    circles.push(index_1.createElement("circle", {
       key: i,
       cx: x,
       cy: y,
       r: "15",
-      fill: "hsl(".concat(hue, ", 70%, 60%)"),
+      fill: "hsl(" + hue + ", 70%, 60%)",
       stroke: "black",
       strokeWidth: "1"
     }));
   }
 
-  return (0, index_1.createElement)("div", null, (0, index_1.createElement)("h2", null, "Stateful SVG Test"), (0, index_1.createElement)("p", null, "Click the links below to add more circles (count: ", props.count, "):"), (0, index_1.createElement)("svg", {
+  return index_1.createElement("div", null, index_1.createElement("h2", null, "Stateful SVG Test"), index_1.createElement("p", null, "Click the links below to add more circles (count: ", props.count, "):"), index_1.createElement("svg", {
     width: "350",
     height: "200",
     style: {
       border: '1px solid black'
     }
-  }, (0, index_1.createElement)("defs", null, (0, index_1.createElement)("pattern", {
+  }, index_1.createElement("defs", null, index_1.createElement("pattern", {
     id: "grid",
     width: "40",
     height: "40",
     patternUnits: "userSpaceOnUse"
-  }, (0, index_1.createElement)("path", {
+  }, index_1.createElement("path", {
     d: "M 40 0 L 0 0 0 40",
     fill: "none",
     stroke: "#ddd",
     strokeWidth: "1"
-  }))), (0, index_1.createElement)("rect", {
+  }))), index_1.createElement("rect", {
     width: "100%",
     height: "100%",
     fill: "url(#grid)"
-  }), circles, (0, index_1.createElement)("a", {
+  }), circles, index_1.createElement("a", {
     href: "#",
     onClick: function onClick(e) {
       e.preventDefault();
       props.buttonClick();
     }
-  }, (0, index_1.createElement)("rect", {
+  }, index_1.createElement("rect", {
     x: "10",
     y: "160",
     width: "60",
@@ -535,19 +541,19 @@ var StatefulSvgTest = function StatefulSvgTest(props) {
     stroke: "blue",
     strokeWidth: "1",
     rx: "5"
-  }), (0, index_1.createElement)("text", {
+  }), index_1.createElement("text", {
     x: "40",
     y: "175",
     textAnchor: "middle",
     fill: "blue",
     fontSize: "12"
-  }, "Add One")), (0, index_1.createElement)("a", {
+  }, "Add One")), index_1.createElement("a", {
     href: "#",
     onClick: function onClick(e) {
       e.preventDefault();
       props.buttonClick();
     }
-  }, (0, index_1.createElement)("rect", {
+  }, index_1.createElement("rect", {
     x: "80",
     y: "160",
     width: "60",
@@ -556,49 +562,45 @@ var StatefulSvgTest = function StatefulSvgTest(props) {
     stroke: "green",
     strokeWidth: "1",
     rx: "5"
-  }), (0, index_1.createElement)("text", {
+  }), index_1.createElement("text", {
     x: "110",
     y: "175",
     textAnchor: "middle",
     fill: "green",
     fontSize: "12"
-  }, "Add More")), (0, index_1.createElement)("text", {
+  }, "Add More")), index_1.createElement("text", {
     x: "175",
     y: "175",
     fill: "black",
     fontSize: "12"
-  }, "Count: ", props.count)), (0, index_1.createElement)("p", null, (0, index_1.createElement)("strong", null, "Demonstrates:")), (0, index_1.createElement)("ul", null, (0, index_1.createElement)("li", null, "SVG namespace working with stateful components"), (0, index_1.createElement)("li", null, "Interactive SVG links calling React-style event handlers"), (0, index_1.createElement)("li", null, "Dynamic rendering based on state (circles increase with count)"), (0, index_1.createElement)("li", null, "All SVG elements properly namespaced despite overlapping HTML tag names (<a>, <text>, etc.)")));
+  }, "Count: ", props.count)), index_1.createElement("p", null, index_1.createElement("strong", null, "Demonstrates:")), index_1.createElement("ul", null, index_1.createElement("li", null, "SVG namespace working with stateful components"), index_1.createElement("li", null, "Interactive SVG links calling React-style event handlers"), index_1.createElement("li", null, "Dynamic rendering based on state (circles increase with count)"), index_1.createElement("li", null, "All SVG elements properly namespaced despite overlapping HTML tag names (<a>, <text>, etc.)")));
 };
 
 exports.StatefulSvgTest = StatefulSvgTest;
 },{"../dist/es6/index":"St5X"}],"t1xc":[function(require,module,exports) {
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+exports.__esModule = true;
 exports.RuntimeErrorTest = exports.NullPropsTest = void 0;
 
 var index_1 = require("../dist/es6/index"); // Test component to test null props handling
 
 
 var NullPropsTest = function NullPropsTest(props) {
-  return (0, index_1.createElement)("div", null, (0, index_1.createElement)("h3", null, "Null Props Test"), (0, index_1.createElement)("p", null, "Props received: ", JSON.stringify(props)), (0, index_1.createElement)("p", null, "Children: ", Array.isArray(props.children) ? props.children.join(', ') : 'none'));
+  return index_1.createElement("div", null, index_1.createElement("h3", null, "Null Props Test"), index_1.createElement("p", null, "Props received: ", JSON.stringify(props)), index_1.createElement("p", null, "Children: ", Array.isArray(props.children) ? props.children.join(', ') : 'none'));
 };
 
 exports.NullPropsTest = NullPropsTest; // Test for the runtime error fix
 
 var RuntimeErrorTest = function RuntimeErrorTest() {
-  return (0, index_1.createElement)("div", null, (0, index_1.createElement)("h2", null, "Runtime Error Fix Test"), (0, index_1.createElement)("p", null, "Testing component with null props but children provided:"), index_1.createElement(exports.NullPropsTest, null, 'Child 1', 'Child 2', 'Child 3'), (0, index_1.createElement)("p", null, "If you can see the content above, the null props issue is fixed!"));
+  return index_1.createElement("div", null, index_1.createElement("h2", null, "Runtime Error Fix Test"), index_1.createElement("p", null, "Testing component with null props but children provided:"), index_1.createElement(exports.NullPropsTest, null, 'Child 1', 'Child 2', 'Child 3'), index_1.createElement("p", null, "If you can see the content above, the null props issue is fixed!"));
 };
 
 exports.RuntimeErrorTest = RuntimeErrorTest;
 },{"../dist/es6/index":"St5X"}],"C3c2":[function(require,module,exports) {
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+exports.__esModule = true;
 exports.Layout = void 0;
 
 var es6_1 = require("../dist/es6");
@@ -612,20 +614,20 @@ var stateful_svg_test_1 = require("./stateful-svg-test");
 var runtime_error_test_1 = require("./runtime-error-test");
 
 var Layout = function Layout(props) {
-  return (0, es6_1.createElement)("div", null, (0, es6_1.createElement)("h1", {
+  return es6_1.createElement("div", null, es6_1.createElement("h1", {
     className: "foo"
-  }, props.title), props.input, (0, es6_1.createElement)("div", null, "count is: ", props.count), (0, es6_1.createElement)("button", {
+  }, props.title), props.input, es6_1.createElement("div", null, "count is: ", props.count), es6_1.createElement("button", {
     onClick: function onClick() {
       return props.buttonClick();
     }
-  }, "add"), (0, es6_1.createElement)("button", {
+  }, "add"), es6_1.createElement("button", {
     onClick: function onClick() {
       return props.buttonClick();
     },
     style: {
       marginLeft: "1em"
     }
-  }, "I also add"), (0, es6_1.createElement)("button", {
+  }, "I also add"), es6_1.createElement("button", {
     onClick: function onClick() {
       return props.buttonClick();
     },
@@ -633,7 +635,7 @@ var Layout = function Layout(props) {
     style: {
       marginLeft: "1em"
     }
-  }, "this is disabled.."), (0, es6_1.createElement)("button", {
+  }, "this is disabled.."), es6_1.createElement("button", {
     onClick: function onClick() {
       return props.buttonClick();
     },
@@ -642,16 +644,16 @@ var Layout = function Layout(props) {
       marginLeft: "1em"
     }
   }, "disabled = false"), props.subComponentText.map(function (t, i) {
-    return (0, es6_1.createElement)(subcomponent_1.SubComponent, {
+    return es6_1.createElement(subcomponent_1.SubComponent, {
       someText: t,
       textAreaChange: function textAreaChange(v) {
         return props.textAreaChange(i, v);
       }
     }, "component ", i, " content");
-  }), "namespaced element (svg):", (0, es6_1.createElement)("div", null, (0, es6_1.createElement)("svg", {
+  }), "namespaced element (svg):", es6_1.createElement("div", null, es6_1.createElement("svg", {
     width: "100",
     height: "100"
-  }, (0, es6_1.createElement)("rect", {
+  }, es6_1.createElement("rect", {
     width: "100%",
     height: "100%",
     style: {
@@ -659,7 +661,7 @@ var Layout = function Layout(props) {
       strokeWidth: 1,
       stroke: 'red'
     }
-  }), (0, es6_1.createElement)("line", {
+  }), es6_1.createElement("line", {
     x1: "0",
     y1: "0",
     x2: "100%",
@@ -668,7 +670,7 @@ var Layout = function Layout(props) {
       strokeWidth: 1,
       stroke: 'red'
     }
-  }), (0, es6_1.createElement)("line", {
+  }), es6_1.createElement("line", {
     x1: "0",
     y1: "100%",
     x2: "100%",
@@ -677,19 +679,17 @@ var Layout = function Layout(props) {
       strokeWidth: 1,
       stroke: 'red'
     }
-  }))), (0, es6_1.createElement)(svg_namespace_test_1.SvgNamespaceTest, null), (0, es6_1.createElement)(stateful_svg_test_1.StatefulSvgTest, {
+  }))), es6_1.createElement(svg_namespace_test_1.SvgNamespaceTest, null), es6_1.createElement(stateful_svg_test_1.StatefulSvgTest, {
     count: props.count,
     buttonClick: props.buttonClick
-  }), (0, es6_1.createElement)(runtime_error_test_1.RuntimeErrorTest, null));
+  }), es6_1.createElement(runtime_error_test_1.RuntimeErrorTest, null));
 };
 
 exports.Layout = Layout;
 },{"../dist/es6":"St5X","./subcomponent":"VMCN","./svg-namespace-test":"G5/u","./stateful-svg-test":"/RRg","./runtime-error-test":"t1xc"}],"Focm":[function(require,module,exports) {
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+exports.__esModule = true;
 
 var index_1 = require("../dist/es6/index");
 
@@ -705,7 +705,7 @@ var buttonClick = function buttonClick() {
 //which means it has its own state, and the user can use ctrl-z etc.
 
 
-var input = (0, index_1.createElement)("input", {
+var input = index_1.createElement("input", {
   ref: function ref(input) {
     input.onkeypress = input.onkeydown = input.onkeyup = input.onchange = function () {
       //do not invoke mount() directly within this handler,
@@ -736,16 +736,16 @@ var subComponentText = ['a component', 'another component'];
 
 function updateRetainFocus() {
   //get the focused element's position and selectionrange
-  var a = (0, index_1.getActiveElementInfo)();
+  var a = index_1.getActiveElementInfo();
   update(); //re-set the focus and selectionrange after the update
 
-  (0, index_1.setActiveElement)(a);
+  index_1.setActiveElement(a);
 }
 
 function update() {
-  (0, index_1.mount)( //note: the layout will detach and append our input above, so it loses its focus and selectionrange.
+  index_1.mount( //note: the layout will detach and append our input above, so it loses its focus and selectionrange.
   //make sure to call getActiveElementInfo prior to calling App
-  (0, layout_1.Layout)({
+  layout_1.Layout({
     title: title,
     count: count,
     buttonClick: buttonClick,
